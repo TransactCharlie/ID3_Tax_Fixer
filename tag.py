9a41706477808c84288252c0ccc0e6a65ec919dd
Charlie Gildawie <charles.gildawie@gmail.com>
2014-02-17 21:25:47 +0000

    tax fixes for mp3

--- tag.py
+++ tag.py
@@ -1,44 +1,50 @@
 __author__ = 'charlie'
 
-from mutagen.easyid3 import ID3
+from mutagen.id3 import ID3, TIT1 as artist, TIT2 as album_artist
 import fnmatch
 import os
 
-
 def file_list(root, pattern):
-    for  r,d,f in os.walk(root):
+    for r,d,f in os.walk(root):
         for fn in fnmatch.filter(f, pattern):
             yield os.path.join(r, fn)
 
+ARTIST_KEY = "TPE1"
+ALBUM_ARTIST_KEY = "TPE2"
 
-def compare_artits_albulm_artist(f):
+def fix_title(track):
+    """@type track: ID3"""
 
-    song = ID3(f)
-
-    if "TPE1" not in song and "TPE2" not in song:
-        print f, "NO ARTIST OR ALBUM ARTIST"
+    # No Author or Album Artist then we can't do anything
+    if ARTIST_KEY not in track and ALBUM_ARTIST_KEY not in track:
         return
 
-    if "TPE1" in song and "TPE2" not in song:
-        # print f, "Album Artist Added"
-        song["TPE2"] = str(song["TPE1"]).strip()
+    if ARTIST_KEY not in track and ALBUM_ARTIST_KEY in track:
+        a = unicode(track[ALBUM_ARTIST_KEY]).strip()
+        print a
+        track[ALBUM_ARTIST_KEY] = album_artist(encoding = 3, text = a)
+        track[ARTIST_KEY] = artist(encoding = 3, text = a)
+        return
 
-    elif "TPE2" in song and "TPE1" not in song:
-        # print f, "Artist Added"
-        song["TPE1"] = str(song["TPE2"]).strip()
+    if ALBUM_ARTIST_KEY not in track and ARTIST_KEY in track:
+        a = unicode(track[ARTIST_KEY]).strip()
+        print a
+        track[ARTIST_KEY] = artist(encoding = 3, text = a)
+        track[ALBUM_ARTIST_KEY] = album_artist(encoding = 3, text = a)
+        return
 
-    if str(song["TPE1"]) != str(song["TPE2"]):
-        # print f, "Preferring Albulm Artist", "[" + str(song["TPE1"]) + "]", "[" + str(song["TPE2"]) + "]"
-        song["TPE1"] = str(song["TPE2"]).strip()
+    a = unicode(track[ARTIST_KEY]).strip()
+    aa = unicode(track[ALBUM_ARTIST_KEY]).strip()
 
-    #cleanup whitespace
-        song["TPE1"] = str(song["TPE1"]).strip()
-        song["TPE2"] = str(song["TPE1"]).strip()
-
-    song.save()
+    # prefer the album artist if its different (unless it Various!)
+    if a != aa and "Various" not in aa:
+        track[ARTIST_KEY] = artist(encoding = 3, text = aa)
 
 if __name__ == "__main__":
-    files = file_list("D:/Media/From Blackberry/", "*.mp3")
+    files = file_list("D:/Media/From Blackberry/Compilations/Wipeout 3/", "*.mp3")
     for f in files:
-        compare_artits_albulm_artist(f)
+        print f
+        t = ID3(f)
+        fix_title(t)
+        t.save()
 
